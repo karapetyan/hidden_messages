52aa55f87e86bb6e821a2a6bacd06ad84acebe78
Sergey Karapetyan <grey.karapetyan@gmail.com>
2018-04-10 22:08:27 +0300

    Hidden messages, nested React component

---
+++ src/components/HiddenMessages/HiddenMessage/HiddenMessage.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class HiddenMessage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: (props.show) ? props.show : false
+        }
+    }
+
+    
+
+    render() {
+        const { children } = this.props
+        const { show } = this.state
+        return (
+            <p>
+                {(show) ?
+                    children : 
+                    children.replace(/[a-zA-Z0-9]/g, "x")
+                }
+            </p>
+        )
+    }
+}
+
+export default HiddenMessage;
